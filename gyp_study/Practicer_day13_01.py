cdcbf7e67bf1f23d0305de924e2ff8fb1e7abe72
gypgong <gongyangping@163.com>
2022-05-05 18:00:18 +0800

    commit gyp_study from gyp

---
+++ gyp_study/Practicer_day13_01.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 构造一个1，3，5，7，9，... ，99的列表，可以通过循环实现：
+L = []
+n = 1
+while n <= 99:
+    L.append(n)
+    n = n + 2
+print(L)
+
+
+
+
+l = list(range(1, 100, 2))
+print(l)
+
