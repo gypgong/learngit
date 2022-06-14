4cdb01b48ba9704d338ac00efd7123d79a9489a0
gypgong <985886185@qq.com>
2022-06-14 16:56:50 +0800

    from gyp commit

--- gyp_study/枚举类.py
+++ gyp_study/枚举类.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from enum import Enum
-from time import clock_settime
 
 Month = Enum('Month',('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
 
@@ -25,3 +24,7 @@ class Weekday(Enum):
     Sat = 6
 
 # @unique 装饰器可以帮助我们检查保证没有重复的值
+# 访问这些枚举类型可以有若干种方法：
+ 
+ day1 = Weekday.Mon
+
