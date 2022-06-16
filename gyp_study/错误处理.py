9b53e606cf0e0fc91aabed6974940a83863aa354
gypgong <985886185@qq.com>
2022-06-16 16:13:16 +0800

    from gyp commit

--- gyp_study/错误处理.py
+++ gyp_study/错误处理.py
@@ -5,7 +5,7 @@
 try:
     
     print('try...')
-    r = 10/0
+    r = 10/2
     print('result:', r)
 except ZeroDivisionError as e:
     
@@ -15,3 +15,51 @@ finally:
     print('finally...')
 
 print('END')
+
+
+# 练习
+# 运行下面代码，根据异常信息进行分析，定位出错误源头，并修复
+from functools import reduce
+
+def str2num(s):
+    return int(s)
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+def main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+main()
+
+
+print('=================分割线=================')
+# 改为
+
+from functools import reduce
+
+def str2num(s):
+    try:
+        return int(s)
+    except ValueError as e:
+        # print('ValueError:', e)
+        return float(s)
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+def main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+main()
+
