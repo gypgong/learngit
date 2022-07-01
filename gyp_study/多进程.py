9b02080eadfead9eed29eb8df7baf46fcb619872
gypgong <985886185@qq.com>
2022-07-01 09:28:01 +0800

    from gyp commiting

---
+++ gyp_study/多进程.py
@@ -0,0 +1,65 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+
+# import multiprocessing
+# import os
+
+# print('Process (%s) start...' % os.getpid())
+# # Only works on Unix/Linux/Mac:
+# pid = os.fork()
+# if pid == 0:
+#     print('我是子进程(%s),我的父进程是(%s).' % (os.getpid(), os.getppid()))
+# else:
+#     print('我(%s)刚刚创建了一个子进程(%s).'% (os.getpid(), pid))
+
+
+
+
+
+# multiprocessing
+
+# from multiprocessing import Process
+# import os
+
+# # 子进程要执行的代码
+# def run_proc(name):
+#     print('运行子进程 %s(%s)' % (name, os.getpid()))
+
+# if __name__=='__main__':
+#     print('父进程 %s.' % os.getpid())
+#     p = Process(target=run_proc, args=('test',))
+#     print('子进程将启动')
+#     p.start()
+#     p.join()
+#     print('子进程结束')
+
+
+
+
+# Pool
+# 如果要启动大量的子进程，可以用进程池的方式批量创建子进程：
+
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+if __name__=='__main__':
+    print('Process process %s.' % os.getpid())
+    p = Pool(14)
+    for i in range(15):
+        p.apply_async(long_time_task, args=(i,))
+    print('Warning for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
+
+
--- gyp_study/序列化.py
+++ gyp_study/序列化.py
@@ -10,3 +10,49 @@ print(data)
 
 reborn = pickle.loads(data)
 print(reborn)
+
+
+
+import json
+d = dict(name='Bob', age=20, score=88)
+data = json.dumps(d)
+print('JSON Data is a str', data)
+reborn = json.loads(data)
+print(reborn)
+
+
+
+class Student(object):
+
+
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+    def __str__(self):
+        return 'Student object (%s, %s, %s)' % (self.name, self.age, self.score)
+
+
+s = Student('Bob', 20, 88)
+std_data = json.dumps(s, default=lambda obj: obj.__dict__)
+print('Dump Student:', std_data)
+rebuild = json.loads(std_data, object_hook=lambda d: Student(d['name'], d['age'], d['score']))
+print(rebuild)
+
+
+
+
+
+import json
+obj = dict(name='小明', age=20)
+
+s = json.dumps(obj)
+print(s)
+
+s1 = json.dumps(obj, ensure_ascii=False)
+print(s1)
+
+s2 = json.dumps(obj, ensure_ascii=True)
+print(s2)
+
